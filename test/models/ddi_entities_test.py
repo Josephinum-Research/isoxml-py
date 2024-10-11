77be61e3938e2df3436cdc902b50c23092ec5fa0
Martin Hungendorfer <martin.hungendorfer@josephinum.at>
2024-10-11 09:58:03 +0200

    introduced DDEntity class

---
+++ test/models/ddi_entities_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from isoxml.models.ddi_entities import DDEntity
+
+
+def test__from_ddi__when_id_known__expect_ddi_entry():
+    entry = DDEntity.from_id(1)
+    assert isinstance(entry, DDEntity)
+
+
+def test__from_ddi__when_id_unknown__expect_error():
+    with pytest.raises(KeyError):
+        DDEntity.from_id(666)
+
+
+def test__to_bytes__when_dde_valid__expect_bytes():
+    entry = DDEntity.from_id(60)
+    assert bytes(entry) == b'\x00\x3c'
+
+
+def test__from_bytes__when_byte_code_known__expect_entry():
+    entry = DDEntity.from_bytes(b'\x00\x3c')
+    assert entry.ddi == 60

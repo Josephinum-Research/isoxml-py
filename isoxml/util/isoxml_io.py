33370ce82df62b7e444c3cdd471bfe21b6714925
Martin Hungendorfer <martin.hungendorfer@josephinum.at>
2024-10-25 13:23:17 +0200

    add export of external file references

--- isoxml/util/isoxml_io.py
+++ isoxml/util/isoxml_io.py
@@ -121,34 +121,51 @@ def isoxml_to_text(task_data: iso3.Iso11783TaskData | iso4.Iso11783TaskData) -> 
 
 
 def isoxml_to_dir(
-        dir_path: Path, task_data: iso3.Iso11783TaskData | iso4.Iso11783TaskData,
-        bin_dict: dict[str, bytes] | None = None
+        dir_path: Path,
+        task_data: iso3.Iso11783TaskData | iso4.Iso11783TaskData,
+        ext_refs: dict[str, bytes | iso3.ExternalFileContents | iso4.ExternalFileContents] | None = None
 ) -> None:
-    if not bin_dict:
-        bin_dict = {}
+    if not ext_refs:
+        ext_refs = {}
     xml_path = dir_path / 'TASKDATA.XML'
     with open(xml_path, 'w', encoding='utf-8') as xml_file:
         __serializer.write(xml_file, task_data)
-    for bin_name, bin_data in bin_dict.items():
-        bin_path = dir_path / (bin_name + '.bin')
-        with open(bin_path, 'wb') as bin_file:
-            bin_file.write(bin_data)
+    for ref_name, ref_data in ext_refs.items():
+        if isinstance(ref_data, bytes):
+            bin_path = dir_path / (ref_name + '.bin')
+            with open(bin_path, 'wb') as bin_file:
+                bin_file.write(ref_data)
+        elif isinstance(ref_data, (iso3.ExternalFileContents, iso4.ExternalFileContents)):
+            xml_path = dir_path / (ref_name + '.XML')
+            with open(xml_path, 'w', encoding='utf-8') as xml_file:
+                __serializer.write(xml_file, ref_data)
+        else:
+            raise ValueError(f'unknown type {type(ref_data)} of external ref {ref_name}')
 
 
 def isoxml_to_zip(
         target,
         task_data: iso3.Iso11783TaskData | iso4.Iso11783TaskData,
-        bin_dict: dict[str, bytes] | None = None,
+        ext_refs: dict[str, bytes | iso3.ExternalFileContents | iso4.ExternalFileContents] | None = None,
         include_folder=True
 ) -> None:
-    if not bin_dict:
-        bin_dict = {}
+    if not ext_refs:
+        ext_refs = {}
     path_in_arch = 'TASKDATA/' if include_folder else ''
     with ZipFile(target, 'w') as zip_archive:
         with zip_archive.open(path_in_arch + 'TASKDATA.XML', 'w') as xml_file:
             xml_content = isoxml_to_text(task_data)
             xml_file.write(xml_content.encode('utf-8'))
-        for bin_name, bin_data in bin_dict.items():
-            bin_filename = bin_name + '.bin'
-            with zip_archive.open(path_in_arch + bin_filename, 'w') as bin_file:
-                bin_file.write(bin_data)
+
+        for ref_name, ref_data in ext_refs.items():
+            if isinstance(ref_data, bytes):
+                bin_filename = path_in_arch + ref_name + '.bin'
+                with zip_archive.open(bin_filename, 'w') as bin_file:
+                    bin_file.write(ref_data)
+            elif isinstance(ref_data, (iso3.ExternalFileContents, iso4.ExternalFileContents)):
+                xml_filename = path_in_arch + ref_name + '.XML'
+                with zip_archive.open(xml_filename, 'w') as xml_file:
+                    xml_content = __serializer.render(ref_data)
+                    xml_file.write(xml_content.encode('utf-8'))
+            else:
+                raise ValueError(f'unknown type {type(ref_data)} of external ref {ref_name}')
